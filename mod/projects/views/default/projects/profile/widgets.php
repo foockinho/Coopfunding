a019e83a95d26595fff164c6521a3e21ca36f7f8
aleph <aleph@riseup.net>
2013-10-25 02:43:28 +0200

    Projects's campaign widget.

--- mod/projects/views/default/projects/profile/widgets.php
+++ mod/projects/views/default/projects/profile/widgets.php
@@ -9,10 +9,9 @@
 echo '<ul id="projects-tools" class="elgg-gallery elgg-gallery-fluid mtl clearfix">';
 
 // enable tools to extend this area
-echo elgg_view("projects/tool_latest", $vars);
+//echo elgg_view("projects/tool_latest", $vars);
 // enable tools to extend this area
 echo elgg_view("projects/campaigns", $vars);
-
 
 echo "</ul>";
 
