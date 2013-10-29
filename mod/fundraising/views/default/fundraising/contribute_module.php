10033e482743151fd5dbc5d8c49c2ac53436a7ea
aleph <aleph@riseup.net>
2013-10-29 01:07:41 +0100

    Make fundcampaign widget not project.

--- mod/fundraising/views/default/fundraising/contribute_module.php
+++ mod/fundraising/views/default/fundraising/contribute_module.php
@@ -11,7 +11,7 @@ elgg_load_library('elgg:fundcampaigns');
 $fundcampaign = fundcampaigns_get_active_campaign ($project->guid);
 
 if (!$fundcampaign) {
-	$content = '<p>' . elgg_echo('rewards:none') . '</p>';
+	$content = '<p>' . elgg_echo('contribution:none') . '</p>';
 } else {	
 
 	$contributors_count = elgg_get_entities(array(
@@ -29,7 +29,7 @@ if (!$fundcampaign) {
 
 	elgg_load_library('coopfunding:fundraising');
 
-	$contributions_amount = fundraising_sum_amount($vars['entity']);
+	$contributions_amount = fundraising_sum_amount($fundcampaign);
 
 	if (!$contributors_count) {
 		$contributors_count = "0";
@@ -37,16 +37,16 @@ if (!$fundcampaign) {
 
 	$body = elgg_view('output/url', array(
 		'text' => elgg_echo('fundraising:contribute:button'),
-		'href' => "fundraising/contribute/{$vars['entity']->guid}",
+		'href' => "fundraising/contribute/{$fundcampaign->guid}",
 		'class' => 'elgg-button elgg-button-action',
 	));
 
 	$amount = elgg_echo('fundraising:contributions:amount', array($contributions_amount));
 	$amount .= elgg_echo('fundraising:contributions:of');
-	$amount .= elgg_echo('fundraising:contributions:eur', array($vars['entity']->total_amount));
+	$amount .= elgg_echo('fundraising:contributions:eur', array($fundcampaign->total_amount));
 
 	if ($vars['entity']->total_amount) {
-		$amount .= "\n" . elgg_echo($contributions_amount / $vars['entity']->total_amount * 100) . '%';
+		$amount .= "\n" . elgg_echo($contributions_amount / $fundcampaign->total_amount * 100) . '%';
 	}
 
 	$body .= "<br>" . elgg_view('output/text', array(
@@ -56,7 +56,7 @@ if (!$fundcampaign) {
 }
 
 echo elgg_view('projects/profile/module', array(
-	'title' => elgg_echo('fundraising:contribute'),
+	'title' => elgg_echo('fundraising:contributions', array($fundcampaign->name)),
 	'content' => $body,
 	'all_link' => $all_link,
 ));
